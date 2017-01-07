78ac489d1dad529b06a6a8856b81661ce6b8da45
russik2013 <z.kon2009@yandex.ru>
2017-01-07 23:33:03 +0300

    ru

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Date: 07.01.2017
  * Time: 23:29
  */
-$verify_token = ""; // Verify token
+$verify_token = "russik"; // Verify token
 if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_REQUEST['hub_verify_token'] == $verify_token) {
     echo $_REQUEST['hub_challenge'];
 }
